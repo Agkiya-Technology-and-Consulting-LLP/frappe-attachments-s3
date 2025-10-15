819ae8e88155ba87de99df1d4bb5f54316e5c71f
Payal Kshirsagar <payalkshirsagar@Payals-MacBook-Pro.local>
2025-10-15 20:09:56 +0530

    add: custom_copy_attachments_from_amended_from

--- frappe_s3_attachment/__init__.py
+++ frappe_s3_attachment/__init__.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 
 __version__ = '0.0.1'
 
+
+from frappe.model.document import Document
+from frappe_s3_attachment.frappe_s3_attachment.custom.document import custom_copy_attachments_from_amended_from
+Document.copy_attachments_from_amended_from = custom_copy_attachments_from_amended_from
--- frappe_s3_attachment/frappe_s3_attachment/custom/document.py
+++ frappe_s3_attachment/frappe_s3_attachment/custom/document.py
@@ -1,29 +1,7 @@
-from frappe import Document
+# from frappe import Document
 import frappe
 
-class CustomDocument(Document):
-	# def copy_attachments_from_amended_from(self):
-	# 	"""Copy attachments from `amended_from`"""
-	# 	from frappe.desk.form.load import get_attachments
-
-	# 	# loop through attachments
-	# 	for attach_item in get_attachments(self.doctype, self.amended_from):
-	# 		# save attachments to new doc
-	# 		_file = frappe.get_doc(
-	# 			{
-	# 				"doctype": "File",
-	# 				"file_url": attach_item.file_url,
-	# 				"file_name": attach_item.file_name,
-	# 				"content_hash": attach_item.content_hash,
-	# 				"attached_to_name": self.name,
-	# 				"attached_to_doctype": self.doctype,
-	# 				"folder": "Home/Attachments",
-	# 				"is_private": attach_item.is_private,
-	# 			}
-	# 		)
-	# 		_file.save()
-   
-	def copy_attachments_from_amended_from(self):
+def custom_copy_attachments_from_amended_from(self):
 		"""Copy attachments from `amended_from`"""
 		from frappe.desk.form.load import get_attachments
 		show_pop_up = False
@@ -36,6 +14,7 @@ class CustomDocument(Document):
 						"doctype": "File",
 						"file_url": attach_item.file_url,
 						"file_name": attach_item.file_name,
+						"content_hash": attach_item.content_hash,
 						"attached_to_name": self.name,
 						"attached_to_doctype": self.doctype,
 						"folder": "Home/Attachments",
